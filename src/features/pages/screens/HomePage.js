16d90eaf34c01d6008aecd3cd20286ac9225bac4
Shivam Chaudhary <shivamc021999@gmail.com>
2021-12-08 19:03:06 +0530

    Home page leaderboard connect, home page code cleanup

--- src/features/pages/screens/HomePage.js
+++ src/features/pages/screens/HomePage.js
@@ -4,20 +4,9 @@ import {connect} from 'react-redux';
 import Colors from '../../../utilities/Colors';
 
 import React, {Component} from 'react';
-import {
-  Image,
-  Text,
-  View,
-  StyleSheet,
-  Platform,
-  ScrollView,
-  Pressable,
-  Linking,
-} from 'react-native';
+import {Image, Text, View, ScrollView, Pressable, Linking} from 'react-native';
 import ScreenStyle from './styles/StylesHomePage';
-import {color} from 'react-native-reanimated';
 
-import LinearGradient from 'react-native-linear-gradient';
 import {LinearTextGradient} from 'react-native-text-gradient';
 import Carousel, {Pagination} from 'react-native-snap-carousel';
 import {Button} from 'react-native-elements';
@@ -134,7 +123,7 @@ class HomePage extends Component {
         }}
         inactiveDotStyle={
           {
-            // Define styles for inactive dots here
+            // Define ScreenStyle for inactive dots here
           }
         }
         inactiveDotOpacity={0.4}
@@ -187,40 +176,54 @@ class HomePage extends Component {
     );
   }
 
+  componentDidMount() {
+    this.props.pagesActions
+      .fetchLeaderboard()
+      .then(data => {})
+      .catch(error => {});
+  }
+
   render() {
+    const leaderboard = this.props.pages.leaderboard;
+    const rank1 = leaderboard.filter(user => user.rank === 1)[0];
+    const rank2 = leaderboard.filter(user => user.rank === 2)[0];
+    const rank3 = leaderboard.filter(user => user.rank === 3)[0];
+    console.log(rank1);
     return (
       // Scroll view starts
       <ScrollView style={ScreenStyle.root}>
         <View style={{paddingTop: 20}}>
           {/* BLITHCHRON  text*/}
           <LinearTextGradient
-            style={styles.textGradientStyle}
+            style={ScreenStyle.textGradientStyle}
             colors={[
               Colors.gradientTextLeft,
               Colors.gradientTextMiddle,
               Colors.gradientTextRight,
             ]}
             locations={[0, 0.5, 1]}>
-            <Text style={styles.blithTitle}>Blithchron</Text>
+            <Text style={ScreenStyle.blithTitle}>Blithchron</Text>
           </LinearTextGradient>
 
           <LinearTextGradient
-            style={styles.textGradientStyle}
+            style={ScreenStyle.textGradientStyle}
             colors={[
               Colors.gradientTextLeft,
               Colors.gradientTextMiddle,
               Colors.gradientTextRight,
             ]}
             locations={[0, 0.5, 1]}>
-            <Text style={styles.blithSubTitle}>A CONFLUENCE OF ECSTASIES</Text>
+            <Text style={ScreenStyle.blithSubTitle}>
+              A CONFLUENCE OF ECSTASIES
+            </Text>
           </LinearTextGradient>
         </View>
 
         {/* UPCOMING EVENTS + CAROUSEL  */}
 
         <View>
-          <Text style={styles.whiteSubTitles}>Upcoming Events</Text>
-          <View style={styles.carouselViewStyle}>
+          <Text style={ScreenStyle.whiteSubTitles}>Upcoming Events</Text>
+          <View style={ScreenStyle.carouselViewStyle}>
             <Carousel
               layout={'default'}
               ref={ref => (this.carousel = ref)}
@@ -238,8 +241,8 @@ class HomePage extends Component {
         {/* PAST EVENTS + CAROUSEL  */}
 
         <View>
-          <Text style={styles.whiteSubTitles}>Past Events</Text>
-          <View style={[styles.carouselViewStyle, {marginBottom: 20}]}>
+          <Text style={ScreenStyle.whiteSubTitles}>Past Events</Text>
+          <View style={[ScreenStyle.carouselViewStyle, {marginBottom: 20}]}>
             <Carousel
               layout={'default'}
               ref={ref => (this.carousel = ref)}
@@ -260,27 +263,33 @@ class HomePage extends Component {
 
         {/* CA LEADREBOARD  */}
 
-        <View style={styles.CALeaderboardView}>
-          <Text style={styles.CALeaderboardTitle}>CA Leaderboard</Text>
+        <View style={ScreenStyle.CALeaderboardView}>
+          <Text style={ScreenStyle.CALeaderboardTitle}>CA Leaderboard</Text>
 
-          <View style={styles.CALeaderboardBoard}>
-            <View style={styles.CALeaderboardRank23}>
+          <View style={ScreenStyle.CALeaderboardBoard}>
+            <View style={ScreenStyle.CALeaderboardRank23}>
               <View style={{alignSelf: 'center'}}>
-                <Text style={styles.rankNumber}>2</Text>
-                <Icon name="chevron-down" style={styles.downIconStyle}></Icon>
+                <Text style={ScreenStyle.rankNumber}>2</Text>
+                <Icon
+                  name="chevron-down"
+                  style={ScreenStyle.downIconStyle}></Icon>
               </View>
 
-              <IonIcon name="person" style={styles.personIconStyle}></IonIcon>
+              <IonIcon
+                name="person"
+                style={ScreenStyle.personIconStyle}></IonIcon>
 
               <View style={{alignItems: 'center'}}>
                 <Text style={{color: 'white'}}>Rank 2</Text>
-                <Text style={{color: 'white'}}>999</Text>
+                <Text style={{color: 'white'}}>
+                  {(rank2 && rank2.points) || '0'}
+                </Text>
               </View>
             </View>
 
-            <View style={styles.CALeaderboardRank1}>
+            <View style={ScreenStyle.CALeaderboardRank1}>
               <View style={{alignSelf: 'center'}}>
-                <Text style={styles.rankNumber1}>1</Text>
+                <Text style={ScreenStyle.rankNumber1}>1</Text>
                 <Icon
                   name="chevron-down"
                   style={{color: 'white', fontSize: 25}}></Icon>
@@ -292,43 +301,51 @@ class HomePage extends Component {
 
               <View style={{alignItems: 'center'}}>
                 <Text style={{color: 'white'}}>Rank 1</Text>
-                <Text style={{color: 'white'}}>9999</Text>
+                <Text style={{color: 'white'}}>
+                  {(rank1 && rank1.points) || '0'}
+                </Text>
               </View>
             </View>
 
-            <View style={styles.CALeaderboardRank23}>
+            <View style={ScreenStyle.CALeaderboardRank23}>
               <View style={{alignSelf: 'center'}}>
-                <Text style={styles.rankNumber}>3</Text>
-                <Icon name="chevron-down" style={styles.downIconStyle}></Icon>
+                <Text style={ScreenStyle.rankNumber}>3</Text>
+                <Icon
+                  name="chevron-down"
+                  style={ScreenStyle.downIconStyle}></Icon>
               </View>
 
-              <IonIcon name="person" style={styles.personIconStyle}></IonIcon>
+              <IonIcon
+                name="person"
+                style={ScreenStyle.personIconStyle}></IonIcon>
 
               <View style={{alignItems: 'center'}}>
                 <Text style={{color: 'white'}}>Rank 3</Text>
-                <Text style={{color: 'white'}}>99</Text>
+                <Text style={{color: 'white'}}>
+                  {(rank3 && rank3.points) || '0'}
+                </Text>
               </View>
             </View>
           </View>
 
-          <View style={styles.buttonBackgroundStyle}>
+          {/* <View style={ScreenStyle.buttonBackgroundStyle}>
             <Button
               title="Know More"
               onPress={() => {
                 Linking.openURL('https://google.com');
               }}
             />
-          </View>
+          </View> */}
         </View>
 
         {/* CONNECT WITH US  */}
 
-        <View style={styles.connectWithUs}>
+        <View style={ScreenStyle.connectWithUs}>
           <View>
-            <Text style={styles.connectWithUsTitle}>Connect with us!</Text>
+            <Text style={ScreenStyle.connectWithUsTitle}>Connect with us!</Text>
           </View>
 
-          <View style={styles.socialLogos}>
+          <View style={ScreenStyle.socialLogos}>
             <Pressable
               onPress={() => {
                 Linking.openURL('https://www.instagram.com/blithchron_iitgn/');
@@ -369,6 +386,19 @@ class HomePage extends Component {
                 style={{
                   fontSize: 30,
                   color: 'white',
+                }}></IonIcon>
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                Linking.openURL(
+                  'https://www.linkedin.com/company/blithchron-iit-gandhinagar/',
+                );
+              }}>
+              <IonIcon
+                name="logo-linkedin"
+                style={{
+                  fontSize: 30,
+                  color: 'white',
                   marginRight: 45,
                 }}></IonIcon>
             </Pressable>
@@ -386,145 +416,5 @@ function mapDispatchToProps(dispatch) {
     pagesActions: bindActionCreators({...pagesActions}, dispatch),
   };
 }
-
-const styles = StyleSheet.create({
-  blithTitle: {
-    marginTop: 16,
-    paddingVertical: 8,
-    // color: "blue",
-    textAlign: 'left',
-    fontSize: 52,
-    fontWeight: 'bold',
-  },
-  blithSubTitle: {
-    marginTop: -10,
-    paddingVertical: 0,
-    color: 'blue',
-    textAlign: 'left',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  whiteSubTitles: {
-    color: 'white',
-    marginTop: 50,
-    paddingVertical: 0,
-    textAlign: 'left',
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginLeft: 20,
-  },
-  CABox: {
-    backgroundColor: 'black',
-    borderRadius: 5,
-    height: 250,
-    padding: 50,
-    marginLeft: 2,
-    marginRight: 30,
-    marginTop: 35,
-  },
-  boxWithShadow: {
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 1},
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 5,
-  },
-  textGradientStyle: {
-    fontWeight: 'bold',
-    fontSize: 20,
-    marginLeft: 20,
-  },
-  carouselViewStyle: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginLeft: 20,
-  },
-  CALeaderboardView: {
-    flexDirection: 'column',
-    backgroundColor: '#0E0E0E',
-    height: 490,
-    width: '90%',
-    marginTop: 20,
-    marginLeft: 20,
-    elevation: 20,
-    shadowColor: 'black',
-  },
-  CALeaderboardTitle: {
-    color: 'white',
-    fontSize: 20,
-    marginBottom: 10,
-    fontWeight: 'bold',
-    marginTop: 10,
-    marginLeft: 10,
-  },
-  CALeaderboardBoard: {
-    height: '80%',
-    flexDirection: 'row',
-  },
-  CALeaderboardRank23: {
-    height: '100%',
-    width: '30%',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-  rankNumber: {
-    color: 'white',
-    alignSelf: 'center',
-    marginTop: 40,
-    fontWeight: 'bold',
-  },
-  downIconStyle: {
-    color: 'white',
-    fontSize: 25,
-  },
-
-  personIconStyle: {
-    fontSize: 40,
-    color: 'white',
-  },
-
-  CALeaderboardRank1: {
-    height: '100%',
-    width: '40%',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-  },
-
-  rankNumber1: {
-    color: 'white',
-    alignSelf: 'center',
-    fontWeight: 'bold',
-  },
-  buttonBackgroundStyle: {
-    width: '40%',
-    alignSelf: 'center',
-    marginTop: -20,
-  },
-  connectWithUs: {
-    flexDirection: 'column',
-    backgroundColor: '#0E0E0E',
-    marginTop: 20,
-    marginLeft: 20,
-    marginRight: 12,
-    marginBottom: 20,
-    elevation: 20,
-    shadowColor: 'black',
-  },
-  connectWithUsTitle: {
-    marginTop: 10,
-    marginLeft: 10,
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  socialLogos: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginTop: 20,
-    marginBottom: 30,
-  },
-});
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
