e48240ad61a18634aafb5fcc4257ada5db757f7b
Shivam Chaudhary <shivamc021999@gmail.com>
2022-01-06 13:20:04 +0530

    Content changes done

---
+++ src/features/pages/screens/styles/StylesAboutUsPage.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../../../utilities/Colors';
+// import DimensionScale from '../../../../utilities/DimensionScale';
+
+let style = {
+  root: {
+    backgroundColor: Colors.white,
+    flex: 1,
+  },
+};
+
+export default StyleSheet.create(style);
